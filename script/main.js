a58df3252a2814204e035e0a8259d33f3d4e60f9
rezwan-mahmud-utso <itz.rezwan69@gmail.com>
2024-10-23 15:34:51 +0600

    Tudo

---
+++ script/main.js
@@ -0,0 +1,56 @@
+let input = document.querySelector(".input")
+let addBtn = document.querySelector(".addbtn")
+let list =document.querySelector("ul")
+let update = document.querySelector(".update")
+
+let tudo =[]
+let id;
+
+
+addBtn.addEventListener("click",()=>{
+  if(input.value){
+    tudo.push(input.value)
+  input.value =""
+  addTudo()
+  }else{
+    alert("Please Enter a value")
+  }
+  
+    
+    
+})
+update.addEventListener("click",()=>{
+  tudo[id]=input.value
+  addTudo()
+
+})
+
+function addTudo(){
+    list.innerHTML =""
+    tudo.map((item)=>{
+      list.innerHTML += `<li> ${item}   <button class="delet">Delet</button><button class="edit">edit</button></li>`
+        
+    })
+  let maindlt =document.querySelectorAll(".delet")
+  let mainedit =document.querySelectorAll(".edit")
+
+  let deletarr= Array.from(maindlt)
+  let editarr= Array.from(mainedit)
+  
+
+  deletarr.map((ditem,index)=>{
+    ditem.addEventListener("click",()=>{
+      tudo.splice(index,1)
+      addTudo()
+    })
+  })
+  editarr.map((eitem ,index)=>{
+    eitem.addEventListener("click",()=>{
+      input.value = tudo[index]
+      id=index
+
+    })
+  })
+  
+
+}
